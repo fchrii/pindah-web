0f676b70c774fe25bb9fce0d163f29e4ea4d548d
Fachri Ahsani <fachriahsani89@gmail.com>
2025-03-19 20:12:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let link = document.getElementById("new-site-link");
+    link.href = "https://techtonics.space";
+    link.setAttribute("target", "_blank"); // Membuka di tab baru
+});
